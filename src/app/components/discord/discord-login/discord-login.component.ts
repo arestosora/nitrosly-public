7edb46c5b681c3dafe394920258680d1a2912efe
A. Mestra <adonymestra2@gmail.com>
2024-03-12 18:01:58 -0500

    Initialization 🎉

---
+++ src/app/components/discord/discord-login/discord-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-discord-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './discord-login.component.html',
+  styleUrl: './discord-login.component.css'
+})
+export class DiscordLoginComponent {
+
+}
